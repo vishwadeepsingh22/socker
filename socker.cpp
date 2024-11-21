8dd252156e80f15a94ec32e9e8720dfc0bdd1781
vishwadeepsingh22 <119534166+vishwadeepsingh22@users.noreply.github.com>
2024-11-21 15:45:42 +0530

    Add files via upload
    first commit

---
+++ socker.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : socker.cpp
+// Author      : Vishwadeep Singh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+string name = "Vishwadeep Singh";
+
+int main(void) {
+	puts("Hello World!!!");
+	cout << "Hello World " << name << "!!" << endl;
+	return EXIT_SUCCESS;
+}
